ecfcc05b914d2bcf44e676a6bbf2578520fbfc57
rcanbaba <rcanbabaoglu@gmail.com>
2022-06-11 09:37:44 +0300

    - Do, try, catch, throws examples completed.

---
+++ SwiftConcurrencyBootcamp/SwiftConcurrencyBootcamp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Can Babaoğlu on 10.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftConcurrencyBootcamp/SwiftConcurrencyBootcamp/SwiftConcurrencyBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyBootcampApp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Can Babaoğlu on 10.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
